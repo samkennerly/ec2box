80480dfdaf5c6b232d9519c2c3eb1c0fa93cab81
Sam Kennerly <samkennerly@gmail.com>
2020-01-13 18:43:14 -0500

    Add small test box

---
+++ src/main.tf
@@ -0,0 +1,17 @@
+
+provider "aws" {
+  profile = var.profile
+  region  = var.region
+  version = "~> 2.44" # AWS plugin version to install
+}
+
+resource "aws_instance" "ec2box" {
+  ami           = lookup(var.amis, var.region)
+  instance_type = var.type
+  tags          = { Name = "ec2box" }
+  volume_tags   = { Name = "ec2box" }
+
+  root_block_device {
+    volume_size = var.gb
+  }
+}
---
+++ src/variables.tf
@@ -0,0 +1,29 @@
+
+variable "amis" {
+  default = {
+    "eu-west-1" = "ami-0713f98de93617bb4"
+    "us-east-1" = "ami-062f7200baf2fa504"
+    "us-west-1" = "ami-03caa3f860895f82e"
+  }
+  description = "Amazon Machine Image for each region"
+}
+
+variable "gb" {
+  default     = "8"
+  description = "Root volume size (GB)"
+}
+
+variable "profile" {
+  default     = "default"
+  description = "AWS profile from ~/.aws/credentials"
+}
+
+variable "region" {
+  default     = "us-east-1"
+  description = "Launch EC2 instance in this AWS region"
+}
+
+variable "type" {
+  default     = "t2.micro"
+  description = "EC2 instance type"
+}
