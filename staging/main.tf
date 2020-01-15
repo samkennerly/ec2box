2709540633c0b957db544106f94a60fbc2718731
Sam Kennerly <samkennerly@gmail.com>
2020-01-15 00:53:54 -0500

    Create aws_key_pair, aws_security_group, user_data template

--- staging/main.tf
+++ staging/main.tf
@@ -1,23 +1,54 @@
-terraform {
-  backend "local" {
-    path = "terraform.tfstate"
+
+locals {
+  folder     = "${path.module}/../etc"
+  public_key = file("${local.folder}/id_rsa.pub")
+  user_data  = templatefile("${local.folder}/user_data", local.user_vars)
+  user_vars  = {
+    awslogs = file("${local.folder}/awslogs.conf")
+    ec2user = "ec2-user"
   }
+}
+
+terraform {
   required_providers {
-    aws = "~> 2.44"
+    aws   = "~> 2.44"
   }
   required_version = "~> 0.12"
 }
-
 provider "aws" {
   profile = var.profile
   region  = var.region
 }
 
+resource "aws_key_pair" "ec2box" {
+  key_name   = var.name
+  public_key = local.public_key
+}
+resource "aws_security_group" "ec2box" {
+  description = "Allow SSH login and all outbound connections"
+  name        = var.name
+
+  egress {
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
+  }
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 22
+    protocol    = "tcp"
+    to_port     = 22
+  }
+}
 resource "aws_instance" "ec2box" {
-  ami           = lookup(var.amis, var.region)
-  instance_type = var.type
-  tags          = { Name = "ec2box" }
-  volume_tags   = { Name = "ec2box" }
+  ami             = lookup(var.amis, var.region)
+  instance_type   = var.type
+  key_name        = aws_key_pair.ec2box.key_name
+  security_groups = [aws_security_group.ec2box.name]
+  tags            = { Name = var.name }
+  user_data       = local.user_data
+  volume_tags     = { Name = var.name }
 
   root_block_device {
     volume_size = var.gb
