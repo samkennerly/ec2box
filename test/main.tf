676131b99465b9ec1478667d6a205a1d93925b32
Sam Kennerly <samkennerly@gmail.com>
2020-01-27 21:40:56 -0500

    Add more test scripts

--- test/main.tf
+++ test/main.tf
@@ -2,15 +2,14 @@ provider "aws" {
   profile = var.profile
   region  = var.region
 }
-
-module "dorothy" {
-  name   = "dorothy"
-  launch = "${path.module}/slippers"
-  source = "./.."
-}
-
 module "leeroy" {
   name    = "leeroy"
   ec2type = "t3.nano"
+  source  = "./.."
+}
+module "dorothy" {
+  name    = "dorothy"
+  install = "${path.module}/dorothy/install"
+  launch  = "${path.module}/dorothy/launch"
   source  = "./.."
 }
