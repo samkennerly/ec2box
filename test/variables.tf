83a8bab434fb18a85b473b220a513ae332c6708c
Sam Kennerly <samkennerly@gmail.com>
2020-02-01 21:45:16 -0500

    Move test variables to terraform.tfvars

--- test/variables.tf
+++ test/variables.tf
@@ -1,8 +1,6 @@
 variable "profile" {
-  default     = "default"
   description = "AWS profile from ~/.aws/credentials"
 }
 variable "region" {
-  default     = "us-east-1"
   description = "Launch ec2box in this AWS region"
 }
